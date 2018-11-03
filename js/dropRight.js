39983044627e9fc9a8fc185ca13fed749c03961e
ivan <ivan.pankov.365@yandex.ru>
2018-11-03 18:58:08 +0300

    03.11.2018/18:58

---
+++ js/dropRight.js
@@ -0,0 +1,8 @@
+function dropUp() {
+    let doc = document.getElementById('rightBar_drop');
+    doc.id = 'rightBar'
+}
+function dropDown() {
+    let doc = document.getElementById('rightBar');
+    doc.id = 'rightBar_drop'
+}
